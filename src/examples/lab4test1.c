a2730d47f02ddd8c40021bfdd962b986b09fc556
Mikael Asplund <mikael.asplund@liu.se>
2020-02-17 14:11:00 +0100

    Added missing parenthesis

--- src/examples/lab4test1.c
+++ src/examples/lab4test1.c
@@ -12,7 +12,7 @@ main (int argc, char *argv[])
 
   for(int i=0;i<argc;i++)
 	{
-	printf("Parameter: %d: %s \n",i,argv[i];
+	  printf("Parameter: %d: %s \n",i,argv[i]);
 	}
 
   return EXIT_SUCCESS;
