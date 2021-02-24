78ed0e1341610574f4ea3a08d9233569e6258bd2
Felipe Boeira <felipe.boeira@liu.se>
2021-02-24 23:43:00 +0100

    Update start_pfs.sh

--- src/userprog/start_pfs.sh
+++ src/userprog/start_pfs.sh
@@ -2,9 +2,9 @@ make -j4
 cd build
 pintos-mkdisk fs.dsk 800
 dd if=/dev/urandom of=random bs=1 count=100
-pintos --qemu -- -f -q
-pintos --qemu -p random -a random --  -q
-pintos --qemu -p ../../examples/pfs -a pfs --  -q
-pintos --qemu -p ../../examples/pfs_writer -a pfs_writer --  -q
-pintos --qemu -p ../../examples/pfs_reader -a pfs_reader --  -q
-pintos --qemu -- run pfs
+pintos --qemu -v -- -f -q
+pintos --qemu -v -p random -a random --  -q
+pintos --qemu -v -p ../../examples/pfs -a pfs --  -q
+pintos --qemu -v -p ../../examples/pfs_writer -a pfs_writer --  -q
+pintos --qemu -v -p ../../examples/pfs_reader -a pfs_reader --  -q
+pintos --qemu -v -- run pfs
