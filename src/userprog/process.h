8ed838730e99c05d2f5427fe9e35cd414dbfbe7c
Gustav Svennas <gussv907@student.liu.se>
2023-01-26 14:45:06 +0100

    Lab 3: Added all the old code from previos year for this lab.

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -2,10 +2,27 @@
 #define USERPROG_PROCESS_H
 
 #include "threads/thread.h"
+#include "threads/synch.h"
+#include "threads/malloc.h"
 
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
+
+/* Struct to replace fn_copy when using thread_create. Contains char *copy
+   to replace fn_copy to setup stack and semaphore block to block the
+   parent thread during setup of child thread. */
+/*struct synch
+/* <<<< Added for lab 3 >>>> */
+/*{
+  char *copy;
+  bool child_load_success;       /* Used to check if load of child process was
+                                    successful. */
+  //struct semaphore parent_block; /* Used to block parent thread while starting
+                                    //child thread. */
+  //struct thread *parent;         /* Keeps track of parent thread. */
+  //struct thread *child;          /* Keeps track of child thread. */
+//};
 
 #endif /* userprog/process.h */
