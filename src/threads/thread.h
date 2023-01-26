a78218da864b8a7a8ca30ac6167b3906af16be84
Gustav Svennas <gussv907@student.liu.se>
2023-01-26 11:23:33 +0100

    Testing ssh

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -95,7 +95,7 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
-    /* Added for lab 1. test*/
+    /* Added for lab 1. */
     struct file *fd_list[130];          /* Struct file pointers to 
                                            all opened files. */
 #endif
