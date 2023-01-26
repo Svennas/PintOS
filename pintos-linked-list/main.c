eae81d24503cf5ef6b68a63fc2a12d5b65c237f8
Gustav Svennas <gussv907@student.liu.se>
2023-01-26 09:47:56 +0100

    Lab 0: Added old solution from previous year and created the runable file. Works as it should.

--- pintos-linked-list/main.c
+++ pintos-linked-list/main.c
@@ -3,23 +3,83 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct student {
+const int SIZE = 30;
+
+typedef struct student {
 	char *name;
 	struct list_elem elem;
-};
+} Student;
 
-
+/* This function fetches a student name from the terminal
+	 output and adds it to the list. */
 void insert (struct list *student_list) {
+	char *input = (char*)malloc(sizeof(char)*SIZE);
+	printf("Write the students name: \n");
+	scanf("%s", input);
+
+	Student *student = malloc(sizeof(Student));
+	student->name = input;
+
+	list_push_back(student_list, &student->elem);
+
 }
 
+/* This function gets a student name from the terminal
+input, removes it from the list, and dellaocates the
+appropriate memory (if the input exists). */
 void delete (struct list *student_list) {
+
+	char *input = (char*)malloc(sizeof(char)*SIZE);
+	printf("Write the name of the student you want to remove: \n");
+	scanf("%s", input);
+
+	struct list_elem *i;
+	i = list_begin(student_list);
+
+	while (i != list_end(student_list)) {
+		Student *student = list_entry (i, Student, elem);
+
+		if (strcmp(student->name, input) == 0) {
+			printf("Deleted student %s\n", student->name);
+			list_remove(i);
+			free(student->name);
+			free(student);
+			free(input);
+			break;
+		}
+		i = list_next(i);
+	}
 }
 
+/* This function prints the entire list. */
 void list (struct list *student_list) {
+
+	struct list_elem *i;
+
+	for (i = list_begin (student_list); i != list_end (student_list);
+			 i = list_next (i))
+	{
+			Student *student = list_entry (i, Student, elem);
+			printf("%s\n", student->name);
+	}
 }
 
+/* This function clears the list and deallocates the memory
+for all items in the list. */
 void quit (struct list *student_list) {
+
+	while (!list_empty (student_list))
+	{
+    struct list_elem *i = list_pop_front(student_list);
+    Student *student = list_entry(i, Student, elem);
+		free(student->name);
+		free(student);
+  }
+
+	printf("Exiting the program...\n");
+	exit(0);
 }
+
 
 int main() {
 	struct list student_list;
@@ -65,6 +125,6 @@ int main() {
 				}
 		}
 	} while(1);
-		
+
 	return 0;
 }
