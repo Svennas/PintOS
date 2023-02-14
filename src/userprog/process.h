6457f083446c63455d7cff884ed52bc5a98fb01b
Gustav Svennas <gussv907@student.liu.se>
2023-02-14 12:11:09 +0100

    Lab 3: Restarted lab 3. Started writing the parent_child struct.

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -8,4 +8,12 @@ int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
 
+struct parent_child {
+    int exit_status;
+    int alive_count;
+    int tid;
+    struct list_elem child;
+    struct parent_child* parent;
+};
+
 #endif /* userprog/process.h */
